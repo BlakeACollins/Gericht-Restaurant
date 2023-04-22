6a55be8f785d67e2348d250a06de995789767803
Blake Collins <blakeac369@gmail.com>
2023-04-21 21:29:13 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navbar } from './components'
+//import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
+import images from '../../constants/images';
+import './Navbar.css';
+
+
+const Navbar = () => {
+    return(
+        <nav className='app_navbar'>
+            
+        </nav>
+    )
+}
+
+
+export default Navbar;
